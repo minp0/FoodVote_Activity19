bee2126a649f0d52b4596803b952ed5cbe24d83b
Wellwhale <minpccl1@gmail.com>
2026-02-01 20:45:05 +0700

    learning callback function, state, props, arrow function

--- src/App.js
+++ src/App.js
@@ -1,22 +1,36 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+
+
+function Food(props) {
+  const {title, callback} = props                       // define props parameter
+  const [name,set_name] = useState("min")               // set state
+
+  const change_name = () => {                           // define function
+    set_name("Inferno")
+    callback("So hungary")                           // set parameter to parent object
+  }
+  
+  return (
+    <div>
+      <a>{title}</a>
+      
+      <button onClick = {change_name}> click here! {name} </button>
+    </div>
+  )
+}
+
 
 function App() {
+  const [food_child,set_child] = useState("Hungry")       // sent setter_child to child function
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+          <Food title="apple" callback={set_child}/> 
+          <a href = "#"> {food_child} </a>
+          <Food title="kiwi" callback={set_child}  /> 
       </header>
     </div>
   );
